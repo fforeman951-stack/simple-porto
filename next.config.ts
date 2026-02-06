336940cd1ff1df44a843df1fce628438b3c62f26
vboxuser <vboxuser@vbox.pmulya.ac.id>
2026-02-06 09:50:00 +0700

    next config build ssg

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  distDir: 'out',
+
   basePath: '/simple-porto',
   assetPrefix: '/simple-porto/',
+
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
